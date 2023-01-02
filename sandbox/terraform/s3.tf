252c716ce71667413ba94d85ddaa463e5d94fa60
Selvakumar Palanisamy <selvakumar.palanisamy@sourcedgroup.com>
2023-01-02 21:10:51 +1100

    custom

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,6 +1,11 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-sandbox1-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "sandbox_bucket1" {
+  bucket = "aft-sandbox2-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
